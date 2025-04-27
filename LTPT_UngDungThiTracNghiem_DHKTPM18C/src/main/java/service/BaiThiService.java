dfbdd3f6dc430fd414f6dc35a076aa11ef92e27f
nttung1011 <121549282+nttung1011@users.noreply.github.com>
2025-04-27 16:59:34 +0700

    thêm phương thức lấy all bài thi

--- LTPT_UngDungThiTracNghiem_DHKTPM18C/src/main/java/service/BaiThiService.java
+++ LTPT_UngDungThiTracNghiem_DHKTPM18C/src/main/java/service/BaiThiService.java
@@ -1,11 +1,12 @@
-package service;
+    package service;
 
-import entities.BaiThi;
+    import entities.BaiThi;
 
-import java.rmi.RemoteException;
-import java.util.List;
+    import java.rmi.RemoteException;
+    import java.util.List;
 
-public interface BaiThiService extends GenericService<BaiThi, Integer>{
-    public List<BaiThi> timDSBaiTHiTheoMaGiaoVien(int maGiaoVien) throws RemoteException;
-    public BaiThi layThongTinChiTietBaiThi(int maBaiThi) throws RemoteException;
-}
+    public interface BaiThiService extends GenericService<BaiThi, Integer>{
+        public List<BaiThi> timDSBaiTHiTheoMaGiaoVien(int maGiaoVien) throws RemoteException;
+        public BaiThi layThongTinChiTietBaiThi(int maBaiThi) throws RemoteException;
+        List<BaiThi> getAllBaiThiForHocSinh(Long maHocSinh) throws RemoteException;
+    }
