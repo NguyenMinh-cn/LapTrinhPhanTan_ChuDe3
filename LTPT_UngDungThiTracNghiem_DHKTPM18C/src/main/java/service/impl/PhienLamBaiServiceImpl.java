e5edef84f28757304962a37499900542e997e167
nttung1011 <121549282+nttung1011@users.noreply.github.com>
2025-04-27 13:18:19 +0700

    sửa PhienLamBaiServiceImpl

--- LTPT_UngDungThiTracNghiem_DHKTPM18C/src/main/java/service/impl/PhienLamBaiServiceImpl.java
+++ LTPT_UngDungThiTracNghiem_DHKTPM18C/src/main/java/service/impl/PhienLamBaiServiceImpl.java
@@ -5,12 +5,27 @@ import entities.PhienLamBai;
 import service.PhienLamBaiService;
 
 import java.rmi.RemoteException;
+import java.util.List;
 
-public class PhienLamBaiServiceImpl extends GenericServiceImpl<PhienLamBai, String> implements PhienLamBaiService {
+public class PhienLamBaiServiceImpl implements PhienLamBaiService {
     private PhienLamBaiDAO phienLamBaiDAO;
 
-    public PhienLamBaiServiceImpl(PhienLamBaiDAO phienLamBaiDAO) throws RemoteException {
-        super(phienLamBaiDAO);
+    public PhienLamBaiServiceImpl(PhienLamBaiDAO phienLamBaiDAO) {
         this.phienLamBaiDAO = phienLamBaiDAO;
+    }
+
+    @Override
+    public List<Object[]> layKetQuaChiTietPhienLamBai(String maPhienLamBai) throws RemoteException {
+        return phienLamBaiDAO.layKetQuaChiTietPhienLamBai(maPhienLamBai);
+    }
+
+    @Override
+    public Object[] tinhDiemVaSoCau(String maPhienLamBai) throws RemoteException {
+        return phienLamBaiDAO.tinhDiemVaSoCau(maPhienLamBai);
+    }
+
+    @Override
+    public List<PhienLamBai> findByMaHocSinh(String maHocSinh) throws RemoteException {
+        return phienLamBaiDAO.findByMaHocSinh(maHocSinh);
     }
 }
